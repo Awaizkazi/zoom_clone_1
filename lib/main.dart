3d3012c9f2019bc52e2a5f24092cc7f4c6873f28
Awaizkazi <Owaizkazi789@gmail.com>
2022-12-08 22:53:42 +0530

    c

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:zoom_clone_1/utils/colors.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +12,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(),
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Colors.blue,
+        scaffoldBackgroundColor: backgroundColor,
+      ),
       home: const Text('Hello, World !'),
     );
   }
